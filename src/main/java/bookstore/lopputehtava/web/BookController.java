7efabed7871df45f3a88d659ca94dbe18a0a12a1
marttyyriroskis <julia.hamalainen@myy.haaga-helia.fi>
2024-09-04 19:36:54 +0300

    Create add and delete book functionalities

--- src/main/java/bookstore/lopputehtava/web/BookController.java
+++ src/main/java/bookstore/lopputehtava/web/BookController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import bookstore.lopputehtava.domain.Book;
 import bookstore.lopputehtava.domain.BookRepository;
 
 @Controller
@@ -23,10 +27,35 @@ public class BookController {
         return "main";
     }
 
-    @GetMapping("/booklist")
+    @GetMapping("/bookList")
     public String showCars(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "bookList";
+    }
+
+    @GetMapping("/newBook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "newBook";
+    }
+
+    @GetMapping("editBook/{id}")
+    public String editBook(@PathVariable("id") Long id, Model model) {
+        // public String editBook(@RequestParam(name = "id") Long id, Model model) {
+        model.addAttribute("editBook", bookRepository.findById(id));
+        return "editBook";
+    }
+
+    @PostMapping("/saveBook")
+    public String saveCar(@ModelAttribute("book") Book book) {
+        bookRepository.save(book);
+        return "redirect:bookList";
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        bookRepository.deleteById(id);
+        return "redirect:/bookList";
     }
 
 }
